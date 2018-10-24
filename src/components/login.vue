2f300983fce9606418e938994a31deba6f464518
ywj <1728914873@qq.com>
2018-10-24 22:13:51 +0800

    add backButton in login.vue and register.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="lg">
-        <form  @submit.prevent="submit">
-            <p><input type="text" placeholder="账号" required></p>
-            <p><input type="password" placeholder="密码" required minlength="8"></p>
-            <p><input type="submit" value="登陆"></p>
-        </form>
+    <div id="container">
+        <router-link to="/">返回</router-link>
+        <div class="lg">
+            <form  @submit.prevent="submit">
+                <p><input type="text" placeholder="账号" required></p>
+                <p><input type="password" placeholder="密码" required minlength="8"></p>
+                <p><input type="submit" value="登陆"></p>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -43,6 +46,18 @@
 </script>
 
 <style scoped>
+    *{
+        text-decoration: none;
+    }
+
+    #container a{
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        margin-left: -175px;
+        margin-top: -195px;
+    }
+
     .lg{
         width: 350px;
         height: 350px;
--- src/components/register.vue
+++ src/components/register.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="re">
-        <form action="#" @submit.prevent="submit">
-            <p><input type="text" placeholder="账号" required></p>
-            <p><input type="password" placeholder="密码" required minlength="8"></p>
-            <p><input type="password" placeholder="再次输入密码" required minlength="8"></p>
-            <p><input type="submit" value="注册"></p>
-        </form>
-    </div>
+   <div id="container">
+       <router-link to="/">返回</router-link>
+       <div class="re">
+           <form action="#" @submit.prevent="submit">
+               <p><input type="text" placeholder="账号" required></p>
+               <p><input type="password" placeholder="密码" required minlength="8"></p>
+               <p><input type="password" placeholder="再次输入密码" required minlength="8"></p>
+               <p><input type="submit" value="注册"></p>
+           </form>
+       </div>
+   </div>
 </template>
 
 <script>
@@ -45,6 +48,17 @@
 </script>
 
 <style scoped>
+    *{
+        text-decoration: none;   
+    }
+    #container a{
+       position: absolute;
+       left: 50%;
+       top: 50%;
+        margin-left: -175px;
+        margin-top: -195px;
+    }
+
     .re{
         width: 350px;
         height: 350px;
