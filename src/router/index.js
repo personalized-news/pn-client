4a532c110a2404139bf4ab2a10d85ffebde69525
ZYSzys <17367077526@163.com>
2018-11-01 14:48:17 +0800

    remove now

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
+import Index from '@/components/index'
 import Login from '@/components/login'
 import register from '@/components/register'
 Vue.use(Router)
