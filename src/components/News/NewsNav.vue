52c03ab58a10092847d17d27adc37fedb7a120af
ywj <1728914873@qq.com>
2018-11-07 23:40:07 +0800

    use vuex

--- src/components/News/NewsNav.vue
+++ src/components/News/NewsNav.vue
@@ -9,19 +9,20 @@
         <el-submenu index="1">
           <template slot="title">
             <i class="el-icon-location"></i>
-            <span>新闻分类</span>
+            <span>国内</span>
           </template>
           <el-menu-item-group>
-            <el-menu-item index="1-1">推荐</el-menu-item>
-            <el-menu-item index="1-2">热点</el-menu-item>
-            <el-menu-item index="1-3">图片</el-menu-item>
-            <el-menu-item index="1-4">科技</el-menu-item>
-            <el-menu-item index="1-5">娱乐</el-menu-item>
-            <el-menu-item index="1-6">游戏</el-menu-item>
-            <el-menu-item index="1-7">体育</el-menu-item>
-            <el-menu-item index="1-8">汽车</el-menu-item>
-            <el-menu-item index="1-9">财经</el-menu-item>
-            <el-menu-item index="1-10">搞笑</el-menu-item>
+            <el-menu-item index="1-1">军事</el-menu-item>
+            <el-menu-item index="1-1">台湾</el-menu-item>
+            <el-menu-item index="1-1">港澳</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>国内焦点</span>
+          </template>
+          <el-menu-item-group>
           </el-menu-item-group>
         </el-submenu>
       </el-menu>
