200cc30993dcfc48fb99e94e8f31f7fba6a319b7
ywj <1728914873@qq.com>
2018-11-15 00:03:04 +0800

    add Pagination

---
+++ src/components/News/PaginationNews.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+  <div class="block">
+    <el-pagination
+      layout="prev, pager, next"
+      :total="newsNumber"
+      :current-page="1"
+      @next-click="change('next')"
+      @prev-click="change('pre')"
+      @current-change="change('other')">
+    </el-pagination>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PaginationNews',
+  props: ['newsNumber'],
+  mounted: function () {
+    console.log(this.newsNumber)
+  },
+  methods: {
+    change: function (kind) {
+      this.$emit('changePage', kind)
+      console.log(event)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /*.container{*/
+    /*position: fixed;*/
+  /*}*/
+</style>
