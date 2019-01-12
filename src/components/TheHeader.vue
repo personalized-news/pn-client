79244aeda470f3cb21425e30d08d2ec399a3e6e8
叶文俊 <13588781920@163.com>
2019-01-12 14:58:41 +0800

    save token in session , username in store (#26)

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,11 +2,11 @@
   <header>
     <p>Personalized News</p>
     <div>
-      <nav v-show="!username">
-        <login-dialog @getName="getUsername"></login-dialog>
+      <nav v-show="!username || !isLogin.status">
+        <login-dialog @getName="getUsername" @loginIn="loginIn"></login-dialog>
         <register-dialog></register-dialog>
       </nav>
-      <div v-show="username" class="userInfo">
+      <div v-show="username && isLogin.status" class="userInfo">
         <span>欢迎您！{{username}}</span>
         <el-button type="text" @click="userLogout" size="medium">登出</el-button>
       </div>
@@ -17,10 +17,11 @@
 import LoginDialog from './LoginDialog'
 import RegisterDialog from './RegisterDialog'
 import { logout } from '@/api/user'
-import { getToken, removeToken } from '@/utils/auth'
+import { removeToken, removeUserName, getUserName } from '@/utils/auth'
 
 export default {
   components: { LoginDialog, RegisterDialog },
+  props: ['isLogin'],
   data: function () {
     return {
       userName: ''
@@ -32,18 +33,24 @@ export default {
     }
   },
   created () {
+    console.log('重新渲染')
     this.getUsername()
   },
   methods: {
     getUsername () {
-      this.userName = getToken() // 在刚登入的时候，如果cookie中的用户信息还在的话,就直接显示登陆状态
+      this.userName = this.$store.getters.userName || getUserName()// 在刚登入的时候，如果cookie中的用户信息还在的话,就直接显示登陆状态
     },
     userLogout () {
+      this.$store.dispatch('setUserName', '')
       removeToken()
+      removeUserName()
       logout()
         .then(res => {
           this.getUsername()
         })
+    },
+    loginIn () {
+      this.isLogin.status = true
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container">
   <div class="top">
-    <the-header :isLogin="isLogin" :username="decodeURIComponent(this.$route.query.userId)"></the-header>
+    <the-header :isLogin="isLogin" ></the-header>
   </div>
   <div class="showNews">
     <NewsNav @get-news="get"></NewsNav>
@@ -20,7 +20,10 @@ import TheHeader from '@/components/TheHeader'
 import NewsNav from '@/components/News/NewsNav'
 import NewsItem from '@/components/News/NewsItem'
 import PaginationNews from '@/components/News/PaginationNews'
-import {getNews} from '@/api/news'
+import { getNews } from '@/api/news'
+import { Message } from 'element-ui'
+import { logout } from '../api/user'
+import { removeToken, removeUserName } from '../utils/auth'
 
 export default {
   name: 'PnIndex',
@@ -34,11 +37,29 @@ export default {
     return {
       showNews: [],
       news: [],
-      newNumber: 10
+      newNumber: 10,
+      isLogin: { status: true }
     }
   },
   created: function () {
     getNews('recommend').then((res) => {
+      if (res.message === 'invalid token') {
+        Message({
+          message: 'Token过期，请重新登陆',
+          type: 'error',
+          duration: 5 * 1000
+        })
+        removeToken() // 移除无效token
+        removeUserName()
+        logout().then(res => {
+          if (res.code === 0) {
+            this.$store.dispatch('setUserName', '') // 把store中的userName置为空串
+            this.isLogin.status = false
+          }
+        }).catch((err) => {
+          this.$message.error(err.toString())
+        })
+      }
       if (res.newsList) {
         this.showNews = res.newsList
         this.$store.dispatch('addNews', res.newsList)
@@ -49,20 +70,11 @@ export default {
   },
   methods: {
     get: function (kind) {
-      console.log(this)
       getNews(kind).then((res) => {
         if (res.newsList) { this.showNews = res.newsList }
       }).catch((err) => {
         this.$message.error(err.toString())
       })
-    }
-  },
-  computed: {
-    isLogin: function () {
-      // 在没有参数传过来时，this.$route.query是空对象
-      // 用isLogin决定顶层的导航显示
-      // 刚进入页面时.this.$route.query是空对象
-      if (this.$route.query.userId === undefined) { return false } else return this.$route.query.userId.length > 0
     }
   },
   destroyed: function () {
